7ea2ad5edd47418223c94243d78b90f6b8a539ae
J-C <jcjoneskern@gmail.com>
2016-11-18 00:25:28 -0500

    added functionality

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 $(function(){
-  //print: title of post, author, number of comments, thumbnail
-  $.get('https://www.reddit.com/r/aww/.json', function(redditData){
+
+  var subreddit;
+  var subredditJson;
+
+  $("form").on("submit", function(event) {
+       event.preventDefault();
+       subreddit = $("#rName").val();
+       subredditJson = "https://www.reddit.com/r/"+subreddit+"/.json"
+       $("#list").html("<h1>Reddit Minus Reddit</h1>");
+  $.get(subredditJson, function(redditData){
     for(var i = 0; i < redditData.data.children.length; i++) {
-      $("#list").append("<img src='" + redditData.data.children[i].data.thumbnail + "'/>"); //thumbnail img
-      $("#list").append("<a href='http://reddit.com"+redditData.data.children[i].data.permalink+ "'>"+redditData.data.children[i].data.title+"</a>");
-      //^this sucks
-      $("#list").append("<p>"+redditData.data.children[i].data.author+"</p>");
-      $("#list").append("<p>"+redditData.data.children[i].data.num_comments+" comments</p>");
+      //for sanity's sake
+      var thumbnail = redditData.data.children[i].data.thumbnail;
+      var title = redditData.data.children[i].data.title;
+      var permalink = redditData.data.children[i].data.permalink;
+      var author = redditData.data.children[i].data.author;
+      var comments = redditData.data.children[i].data.num_comments;
+      //this is hard to read
+      $("#list").append("<div><img src='" + thumbnail + "'/><a href='http://reddit.com"+permalink+ "'>"+title+"</a><p>"+author+"</p><p>"+comments+" comments</p></div>"
+        );
     }
   });
 
+  });
 });
